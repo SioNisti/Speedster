ea0a19d129a012ce624cf0908fbbdb25f40bd867
SioN <SioN@siontea.com>
2022-03-04 09:22:46 +0200

    init

---
+++ speedster/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace speedster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showHighestSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetHighestSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeColorDependingOnSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 74.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-20, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 113);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "000Km/h";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 74.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(-20, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(444, 113);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "000Km/h";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label2_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showHighestSpeedToolStripMenuItem,
+            this.resetHighestSpeedToolStripMenuItem,
+            this.changeColorDependingOnSpeedToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(257, 92);
+            // 
+            // showHighestSpeedToolStripMenuItem
+            // 
+            this.showHighestSpeedToolStripMenuItem.CheckOnClick = true;
+            this.showHighestSpeedToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.showHighestSpeedToolStripMenuItem.Name = "showHighestSpeedToolStripMenuItem";
+            this.showHighestSpeedToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
+            this.showHighestSpeedToolStripMenuItem.Text = "Show highest speed";
+            this.showHighestSpeedToolStripMenuItem.CheckedChanged += new System.EventHandler(this.showHighestSpeedToolStripMenuItem_CheckedChanged);
+            // 
+            // resetHighestSpeedToolStripMenuItem
+            // 
+            this.resetHighestSpeedToolStripMenuItem.Name = "resetHighestSpeedToolStripMenuItem";
+            this.resetHighestSpeedToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
+            this.resetHighestSpeedToolStripMenuItem.Text = "Reset highest speed";
+            this.resetHighestSpeedToolStripMenuItem.Click += new System.EventHandler(this.resetHighestSpeedToolStripMenuItem_Click);
+            // 
+            // changeColorDependingOnSpeedToolStripMenuItem
+            // 
+            this.changeColorDependingOnSpeedToolStripMenuItem.CheckOnClick = true;
+            this.changeColorDependingOnSpeedToolStripMenuItem.Name = "changeColorDependingOnSpeedToolStripMenuItem";
+            this.changeColorDependingOnSpeedToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
+            this.changeColorDependingOnSpeedToolStripMenuItem.Text = "Change color depending on speed";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 114);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "SioN\'s Kmh meter";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showHighestSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetHighestSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeColorDependingOnSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ speedster/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace speedster
+{
+
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        Mem m = new Mem();
+        
+        string speedAddress = "RallyTrophy.exe + 0x00163FDC, 284";
+        string igtAddress = "RallyTrophy.exe + 0x000AABDC, 4";
+        double ts = 0.00d;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            int PID = m.GetProcIdFromName("RallyTrophy");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                timer1.Interval = 1;
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double a = m.ReadFloat(speedAddress, "", false);
+            string igt = m.ReadString(igtAddress);
+
+            if (igt == "00:00:00")
+            {
+                ts = 0.00d;
+                label2.Text = "000Km/h";
+            }
+
+            if (a > ts)
+            {
+                ts = a;
+                label2.Text = a.ToString("000") + "Km/h";
+            }
+
+            if (changeColorDependingOnSpeedToolStripMenuItem.Checked)
+            {
+                if (a < 30)
+                {
+                    label1.ForeColor = Color.Yellow;
+                }
+                else if (a > 30 && a < 50)
+                {
+                    label1.ForeColor = Color.Orange;
+                }
+                else if (a > 50 && a < 60)
+                {
+                    label1.ForeColor = Color.Red;
+                }
+                else if (a > 60 && a < 70)
+                {
+                    label1.ForeColor = Color.DarkRed;
+                }
+                else if (a >= 70)
+                {
+                    label1.ForeColor = Color.Black;
+                }
+            }
+            else
+            {
+                label1.ForeColor = Color.Black;
+            }
+
+            if (a <= -1)
+            {
+                label1.Text = a.ToString("00") + "Km/h";
+            } else
+            {
+                label1.Text = a.ToString("000") + "Km/h";
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void showHighestSpeedToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            if (showHighestSpeedToolStripMenuItem.Checked)
+            {
+                this.Size = new Size(395, 216);
+            } else if (!showHighestSpeedToolStripMenuItem.Checked)
+            {
+                this.Size = new Size(395, 108);
+            }
+        }
+
+        private void resetHighestSpeedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ts = 0.00d;
+            label2.Text = "000Km/h";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+    }
+}
